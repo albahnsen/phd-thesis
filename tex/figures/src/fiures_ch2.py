22dc1c2f3c9af627fc1c1278dd6fb3efb478013d
AlBahnsen <al.bahnsen@gmail.com>
2015-03-04 17:49:35 +0100

    ch2 update features

--- tex/figures/src/fiures_ch2.py
+++ tex/figures/src/fiures_ch2.py
@@ -11,7 +11,7 @@ path = "/home/al/DriveAl/Publications/2015/phd-thesis/tex/figures/"
 col_pal = sns.color_palette()
 
 ############################################################################
-# ch2_fig1 Classification example
+# ch2_fig1 & ch2_fig2 Classification example
 ############################################################################
 
 # from  http://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
@@ -43,10 +43,41 @@ ax1.tick_params(axis='both', which='major', labelsize=8)
 plt.savefig(path+'ch2_fig1.eps', format='eps', dpi=1000, bbox_inches='tight')#, pad_inches=0)
 plt.show()
 
-# Cross table
+# Cross table 1
 TP = df.Y.sum() - 5
 FP = 4
 FN = 5
 TN = df.shape[0] - TP - FP - FN
-
 print TP, FP, FN, TN
+
+# ch2_fig2
+f, ax1 = plt.subplots(1, 1, figsize=(4, 2.5))
+df_temp = df.loc[df.Y == 1]
+ax1.scatter(df_temp["sepal_length"], df_temp["sepal_width"], c=col_pal[2], label="Positive")
+df_temp = df.loc[df.Y == 0]
+ax1.scatter(df_temp["sepal_length"], df_temp["sepal_width"], c=col_pal[0], label="Negative")
+# create step classifier
+b = (5-2.1) / (7.8-4)
+a = 2.1 - b*4
+ax1.plot([4.7, 5.63], [b*4.7+a, b*5.63+a], '--', c="black")
+ax1.plot([4.7, 4.7], [b*4.7+a, 0], '--', c="black")
+ax1.plot([5.63, 5.63], [b*5.63+a, b*5.63+a+0.55], '--', c="black")
+ax1.plot([5.63, 7.5], [b*5.63+a+0.55, b*5.63+a+0.55], '--', c="black")
+ax1.set_xlabel('Feature 1', fontsize=8)
+ax1.set_ylabel('Feature 2', fontsize=8)
+ax1.set_ylim(2, 4.5)
+ax1.set_xlim(4, 7.5)
+ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=8)
+ax1.tick_params(axis='both', which='major', labelsize=8)
+plt.savefig(path+'ch2_fig2.eps', format='eps', dpi=1000, bbox_inches='tight')#, pad_inches=0)
+plt.show()
+# Cross table 2
+TP = df.Y.sum() - 4
+FP = 2
+FN = 4
+TN = df.shape[0] - TP - FP - FN
+print TP, FP, FN, TN
+
+
+
+
